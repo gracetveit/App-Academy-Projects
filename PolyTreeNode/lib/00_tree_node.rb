3c79ccf812ae37c2080a8d472cc6a4e83c01e817
Grace Tveit <grace.tveit@gmail.com>
2020-12-23 12:05:55 -0800

    Added Init and parent= to PolyTreeNode class

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,20 @@
 class PolyTreeNode
+    attr_reader :value, :parent
+    attr_accessor :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
 
+    def parent=(parent)
+        
+        if parent != nil && !parent.children.include?(self)
+            if @parent != nil
+                @parent.children.delete(self)
+            end
+            parent.children << self
+        end
+        @parent = parent
+    end
 end
