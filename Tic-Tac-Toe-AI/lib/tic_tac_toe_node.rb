1f7918165425b6047ec3a08ff2a85dbc21888bed
Grace Tveit <grace.tveit@gmail.com>
2020-12-28 10:54:01 -0800

    Added #children method to tic_tac_toe_node.rb

--- Tic-Tac-Toe-AI/lib/tic_tac_toe_node.rb
+++ Tic-Tac-Toe-AI/lib/tic_tac_toe_node.rb
@@ -1,7 +1,7 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board, :next_mover_mark
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -17,5 +17,29 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    new_arr = []
+
+    # Every empty node do: make a dupe board w/ `next mover mark` in the
+    # pos. Alternate `next_mover_mark`, and set `prev_move_pos` to the pos
+    # marked.
+    @board.rows.each_with_index do |row, y|
+      row.each_with_index do |e, x|
+        pos = [x, y]
+        if @board.empty?(pos)
+          new_board = @board.dup
+          new_board[pos] = next_mover_mark
+
+          if next_mover_mark == :o
+            new_mover_mark = :x
+          else
+            new_mover_mark = :o
+          end
+
+          new_arr << TicTacToeNode.new(new_board, new_mover_mark, pos)
+        end
+      end
+    end
+
+    new_arr
   end
 end
