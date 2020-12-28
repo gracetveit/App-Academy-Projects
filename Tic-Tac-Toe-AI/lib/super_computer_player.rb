2ced8c8f582a5166be037546a1440b4fadbea610
Grace Tveit <grace.tveit@gmail.com>
2020-12-28 11:25:28 -0800

    Added a super_computer_player subclass

--- Tic-Tac-Toe-AI/lib/super_computer_player.rb
+++ Tic-Tac-Toe-AI/lib/super_computer_player.rb
@@ -2,6 +2,18 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    game_tree = new TicTacToeNode(game, mark)
+    game_tree.children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+    game_tree.children.each do |child|
+      if !child.losing_node?(mark)
+        return child.prev_move_pos
+      end
+    end
+    raise "Error, the only winning move is not to play."
   end
 end
 
