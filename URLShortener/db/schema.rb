cda1a8250d9deb94b93ee1a7cbf651953fafd9a5
Grace Tveit <grace.tveit@gmail.com>
2021-01-18 13:11:22 -0800

    Added migration for tagtopic, and taggings

--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_001433) do
+ActiveRecord::Schema.define(version: 2021_01_18_210901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,22 @@ ActiveRecord::Schema.define(version: 2021_01_18_001433) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
     t.index ["user_id"], name: "index_shortened_urls_on_user_id"
+  end
+
+  create_table "tag_topics", force: :cascade do |t|
+    t.string "topic", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["topic"], name: "index_tag_topics_on_topic", unique: true
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_topic_id", null: false
+    t.integer "shortened_url_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["shortened_url_id"], name: "index_taggings_on_shortened_url_id"
+    t.index ["tag_topic_id"], name: "index_taggings_on_tag_topic_id"
   end
 
   create_table "users", force: :cascade do |t|
