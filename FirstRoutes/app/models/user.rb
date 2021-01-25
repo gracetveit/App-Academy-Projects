fa8a1032bb1ebc63f0ee5a19674b7792cb12719d
Grace Tveit <grace.tveit@gmail.com>
2021-01-25 15:09:59 -0800

    Initialized database schema and User model

---
+++ FirstRoutes/app/models/user.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer(8)      not null, primary key
+#  name       :string          not null
+#  email      :string          not null
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+end
