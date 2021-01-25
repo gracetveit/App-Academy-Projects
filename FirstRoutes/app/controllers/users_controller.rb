1734df613b95f5e310bff20265d932b25188eb62
Grace Tveit <grace.tveit@gmail.com>
2021-01-25 15:19:46 -0800

    Added #path_params to ensure proper param entry, and #create to handle POST requests

--- FirstRoutes/app/controllers/users_controller.rb
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all
+    end
+
+    def create
+        user = User.new(path_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, 
+                status: :unprocessable_entity
+        end
+    end
+
+    def path_params
+        params.require(:user).permit(:id, :name, :email)
     end
 end
