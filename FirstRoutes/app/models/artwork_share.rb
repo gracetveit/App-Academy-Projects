66390120772134985458fed3352c5db0e8e3ccf5
Grace Tveit <grace.tveit@gmail.com>
2021-01-27 14:57:13 -0800

    Aded ArtworkShare associations

--- FirstRoutes/app/models/artwork_share.rb
+++ FirstRoutes/app/models/artwork_share.rb
@@ -10,4 +10,27 @@
 #
 
 class ArtworkShare < ApplicationRecord
+    validates :artwork_id, presence:true
+    validates :viewer_id, presence:true
+    validate :unique_id_share
+
+    def unique_id_share
+        if !ArtworkShare.all.
+            where("artwork_id = ? and viewer_id = ?",
+                self.artwork_id,
+                self.viewer_id
+        ).empty?
+            errors.add("This artwork has already been shared to this user")
+        end
+    end
+
+    belongs_to :shared_viewers,
+        primary_key: :id,
+        foreign_key: :viewer_id,
+        class_name: :User
+
+    belongs_to :shared_artworks,
+        primary_key: :id,
+        foreign_key: :artwork_id,
+        class_name: :Artwork
 end
