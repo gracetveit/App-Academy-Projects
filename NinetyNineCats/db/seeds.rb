72e4132d2fb55f138fff594a8468e57333345e79
Grace Tveit <grace.tveit@gmail.com>
2021-02-03 11:37:56 -0800

    Added test cat seeds

--- NinetyNineCats/db/seeds.rb
+++ NinetyNineCats/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+
+cat1 = Cat.create(
+    name: "Purrl",
+    birth_date: Date.new(2005, 1, 1),
+    color: "White",
+    sex: "F",
+    description: "Queen Shit"
+)
