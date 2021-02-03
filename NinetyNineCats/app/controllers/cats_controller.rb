2de099a2502514cd0ba4af323a975d25173f1026
Grace Tveit <grace.tveit@gmail.com>
2021-02-03 14:26:16 -0800

    Added functionality to create a new cat with an html form

--- NinetyNineCats/app/controllers/cats_controller.rb
+++ NinetyNineCats/app/controllers/cats_controller.rb
@@ -9,6 +9,21 @@ class CatsController < ApplicationController
         render :show
     end
 
+    def new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+            puts @cat.errors.full_messages
+        end
+    end
+
     private
 
     def cat_params
