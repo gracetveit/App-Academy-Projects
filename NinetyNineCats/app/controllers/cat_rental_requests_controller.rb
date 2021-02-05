12c4e7fdf8acfd0a6ce9ba93619525d056606a3c
Grace Tveit <grace.tveit@gmail.com>
2021-02-05 10:05:31 -0800

    Added buttons for approving/denying requests on each cat's page

--- NinetyNineCats/app/controllers/cat_rental_requests_controller.rb
+++ NinetyNineCats/app/controllers/cat_rental_requests_controller.rb
@@ -13,6 +13,20 @@ class CatRentalRequestsController < ApplicationController
         end
     end
 
+    def approve
+        @cat_rental_request = CatRentalRequest.find_by(id: params[:id])
+        @cat_rental_request.approve!
+        redirect_to cat_url(Cat.find_by(id: @cat_rental_request.cat_id))
+    end
+
+    def deny
+        @cat_rental_request = CatRentalRequest.find_by(id: params[:id])
+        @cat_rental_request.deny!
+        redirect_to cat_url(Cat.find_by(id: @cat_rental_request.cat_id))
+    end
+
+    private
+
     def cat_rental_request_params
         params.require(:cat_rental_requests).permit(
             :cat_id,
--- NinetyNineCats/config/routes.rb
+++ NinetyNineCats/config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cats, only: [:index, :show, :new, :create, :edit, :update]
   resources :cat_rental_requests, only: [:new, :create]
+  resources :cat_rental_requests do
+    member do
+      get 'approve'
+      get 'deny'
+    end
+  end
 end
