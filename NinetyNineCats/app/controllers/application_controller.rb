9352e15f6c6a88413f3b3b03b09c860359e4716c
Grace Tveit <grace.tveit@gmail.com>
2021-02-11 15:19:19 -0800

    Added session support

--- NinetyNineCats/app/controllers/application_controller.rb
+++ NinetyNineCats/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    helper_method :login!, :current_user
+
+    def login!(user)
+        @user = user
+        session[:session_token] = user.session_token
+    end
+
+    def current_user
+        return nil if session[:session_token].nil?
+        @user ||= User.find_by(session_token: session[:session_token])
+    end
 end
--- NinetyNineCats/app/controllers/sessions_controller.rb
+++ NinetyNineCats/app/controllers/sessions_controller.rb
@@ -5,12 +5,17 @@ class SessionsController < ApplicationController
     def create
         user_name = sessions_params["user_name"]
         password = sessions_params["password"]
+        
         @user = User.find_by_credentials(user_name, password)
         @user.reset_session_token!
+        login!(@user)
         redirect_to cats_url
     end
 
     def destroy
+        current_user.reset_session_token! if current_user
+        session[:session_token] = nil
+        redirect_back fallback_location: cats_url
     end
 
     private
