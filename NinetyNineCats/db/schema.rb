1d825805a2e68c8e297db3ce9e7bf22c71bd753d
Grace Tveit <grace.tveit@gmail.com>
2021-02-11 12:54:15 -0800

    Added User model and table

--- NinetyNineCats/db/schema.rb
+++ NinetyNineCats/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_200407) do
+ActiveRecord::Schema.define(version: 2021_02_11_205109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2021_02_04_200407) do
     t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end
